55838e64d3bbade792c02ad393d962932e9eed99
Mr. Habibi <rizkyhabibi09@gmail.com>
2019-01-30 16:06:39 +0700

    better regex

--- neuro/src/main/java/com/bukalapak/neuro/Utils.kt
+++ neuro/src/main/java/com/bukalapak/neuro/Utils.kt
@@ -16,17 +16,11 @@ val UNPATTERNED_REGEX = """<\w+>""".toRegex()
 val PATTERNED_REGEX = """<\w+:[^>]+>""".toRegex()
 
 val ANY_VARIABLE = """<[^>]*>""".toRegex()
-val LITERAL_STRING_REGEX = """(^|>)([^<]+)""".toRegex()
+val LITERAL_STRING_REGEX = """([^>]+)(?=<|${'$'})""".toRegex()
 val VARIABLE_ABLE_REGEX = """<(\w+)(:([^>]+))?>""".toRegex()
 
 internal fun String.toPattern(): String {
-    return this.replace(LITERAL_STRING_REGEX) {
-
-        // add literal string boundary
-        val prefix = it.groups[1]?.value ?: ""
-        val value = it.groups[2]?.value ?: ""
-        """$prefix\Q$value\E"""
-    }
+    return this.replace(LITERAL_STRING_REGEX) { """\Q${it.value}\E""" }
             .replace(ANONYMOUS_REGEX, COMMON_PATTERN) // unspecified regex
             .replace(UNPATTERNED_REGEX, "($COMMON_PATTERN)") // variable with no specific regex
             .replace(VARIABLE_ABLE_REGEX, "($3)") // variable with specific regex
